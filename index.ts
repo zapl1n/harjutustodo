92adf18d84007e2e9a27c41024f3429fab4cda66
zapl1n <zapl1n@github.com>
2023-04-24 19:02:12 +0300

    As a user I can sign up [Delivers #185018566]

--- index.ts
+++ index.ts
@@ -1,20 +1,22 @@
 // Add express
 import express from 'express'
-const app  = express()
+const app = express()
 
+// Add bcrypt
+import bcrypt from 'bcrypt'
 
-//Add prisma
-
+// Add prisma
 import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
 
+// Parse application/json
+app.use(express.json())
 
 // Add dotnev
 import dotenv from 'dotenv'
 dotenv.config()
 
-//Add Swagger
+// Add Swagger
 import swaggerUi from 'swagger-ui-express'
 import yamljs from 'yamljs'
 const swaggerDocument = yamljs.load('./swagger.yaml')
@@ -23,10 +25,43 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // Serve static files
 app.use(express.static('public'))
 
-//set port
+// Set port
 const port = process.env.Port || 3000
 
-//Listen to port
-app.listen(port,()=>{
+app.post('/users', async (req, res) => {
+
+  // Validate email and password
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send('Email and password required')
+  }
+
+  // Validate that email is unique
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email: req.body.email
+    }
+  })
+
+  if (userExists) {
+    return res.status(409).send('Email already exists')
+  }
+
+  // Hash password
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+  // Create user
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      password: hashedPassword,
+    }
+  })
+
+  // Return user
+  res.status(201).end()
+})
+
+// Listen to port
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}. Documentation at http://localhost:${port}/docs`)
 })
